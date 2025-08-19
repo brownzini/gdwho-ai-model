f1d9a05e55ab00a01971ae79c3ecd9b88965c295
GabrielDavid1 <biocamendeschico@gmail.com>
2025-08-19 14:31:54 -0300

    [Feature]: make services struct

--- app/routes/train.py
+++ app/routes/train.py
@@ -1,10 +1,13 @@
 from fastapi import APIRouter, HTTPException
 
+from app.model.schemas import TrainingRequest
+from app.services.model_service import get_model
+
 router = APIRouter()
 
 @router.post("")
-def train():
+def train(request: TrainingRequest):
     try:
-        return {"message": f"train success"}
+        return get_model(request.id)
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
---
+++ app/services/model_service.py
@@ -0,0 +1,2 @@
+def get_model(model_id: int) -> str:
+    return f"Modelo buscado no indice -> {model_id}"
